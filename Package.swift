43d0ecc23a52318425accedf5f9b08d5df318c9b
Gavin Eadie <gavin@umich.edu>
2025-04-07 23:36:45 -0400

    make logging depend on platform ..

--- Package.swift
+++ Package.swift
@@ -5,8 +5,15 @@ import PackageDescription
 let package = Package(
     name: "ProcessDownLists",
     platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
+    ],
     targets: [
         .executableTarget(
-            name: "ProcessDownLists"),
+            name: "ProcessDownLists",
+            dependencies: [
+                .product(name: "Logging", package: "swift-log",
+                         condition: .when(platforms: [.linux]))
+            ]),
     ]
 )
